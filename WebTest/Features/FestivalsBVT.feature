58f0ec7ed845106ec58ee6304e49d5853da6d5e0
Mat Walker <mat@alexview.com>
2021-06-24 08:16:49 +1000

    New WebTest project. Specflow MSTest framework and using Selenium.  As this is for a quick 'Coding' skills test against a simple non-graphical website no external libraries, such as TeamControllium, will be used.  User must be aware that this will therefore NOT be extentensible if target application should grow.

---
+++ WebTest/Features/FestivalsBVT.feature
@@ -0,0 +1,12 @@
+﻿Feature: FestivalsBVT
+	Basic Validation Tests to verify Festivals page is functioning.  Correctness of functioning is NOT tested.
+	***Background***: **[Original definition for creation of this test suite](https://eacp.energyaustralia.com.au/codingtest/testers**
+
+@BVT
+@Connected
+Scenario: Page is populated with something
+
+
+@BVT
+@Stubbed
+Scenario: Page is populated with injected basic data
---
+++ WebTest/Steps/FestivalsStepDefinitions.cs
@@ -0,0 +1,37 @@
+﻿using TechTalk.SpecFlow;
+
+namespace WebTest.Steps
+{
+    [Binding]
+    public sealed class FestivalsStepDefinitions
+    {
+
+        private readonly ScenarioContext _scenarioContext;
+
+        public FestivalsStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        /// <summary>
+        /// Required Festivals URL is entered in current Browser.
+        /// </summary>
+        /// <remarks>
+        /// URL is obtained from the execution data as this may change depending on environment (IE. Test, Integration, Production, Stubbed, Connected etc....)
+        /// </remarks>
+        [Given("i browse to the Festivals page")]
+        public void GivenPageIsBrowsedTo()
+        {
+            _scenarioContext.Pending();
+        }
+
+        /// <summary>
+        /// Verify that the page contains data.  The data, formatting etc are NOT verified.  Verification is to to verify that at least one festival is shown.
+        /// </summary>
+        [Then("the page is populated with some festivals")]
+        public void TheSomeFestivalsAreShown()
+        {
+            _scenarioContext.Pending();
+        }
+    }
+}
